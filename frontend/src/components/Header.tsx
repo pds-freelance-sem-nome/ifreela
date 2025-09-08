efd91665d15718214957b504640fe5574e7a97f2
V-Matheus <victormatheus507@gmail.com>
2025-09-08 09:50:51 -0300

    feat: add Header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '@/components/Button';
+
+interface HeaderProps {
+  isPortal?: boolean;
+}
+
+export function HeaderNav({ isPortal }: { isPortal?: boolean }) {
+  return (
+    <>
+      {isPortal ? (
+        <nav className="flex gap-4 text-xl leading-6">
+          <Link href="/demandas">Demandas</Link>
+          <Link href="/servicos">Serviços</Link>
+          <Link href="/suporte">Suporte</Link>
+        </nav>
+      ) : (
+        <nav className="flex gap-4 text-xl leading-6">
+          <Link href="/dashboard">Dashboard</Link>
+          <Link href="/servicos">Serviços</Link>
+          <Link href="/demandas">Demandas</Link>
+        </nav>
+      )}
+    </>
+  );
+}
+
+export function Header({ isPortal }: HeaderProps) {
+  return (
+    <header className="flex justify-between items-center px-20 py-12">
+      <Image src="/logo.svg" alt="Logo" width={200} height={48} />
+      <HeaderNav isPortal={isPortal} />
+      <div className="flex space-x-3">
+        <Button variant="outline">Entrar</Button>
+        <Button>Cadastrar</Button>
+      </div>
+    </header>
+  );
+}
